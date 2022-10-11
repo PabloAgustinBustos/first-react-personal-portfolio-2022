4d74e9ba165c6e3b40fe16ca435e9754bfd30bf3
Pablo <pablobustos609@gmail.com>
2022-10-11 01:04:47 -0300

    added a loader on the buttons of the skills page in order to give feedback to the user

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import {motion} from "framer-motion"
 import s from "./styles.module.css"
 
-const Button = ({id, text, form, onClick}) => <motion.button id={id} className={`${s.button} ${s[form]}`} onClick={onClick}>{text}</motion.button>
+const Button = ({id, text, form, onClick}) => (<>
+
+    
+    <motion.button id={id} className={`${s.button} ${s[form]}`} onClick={onClick}>
+        {form === "selected" ? <div className={s.loader}></div> : null}
+    
+        {text}
+    </motion.button>
+</>)
 
 export default Button
